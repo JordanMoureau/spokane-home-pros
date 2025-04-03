1b2ab1b50187bb225e9551714b9a83a9e9df5038
Jordan Moureau <jordanfromnovember@gmail.com>
2025-04-03 14:00:49 -0700

    safari font fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Inter, Raleway } from "next/font/google";
+import { Html, Head, Main, NextScript } from "next/document";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Raleway:wght@400;700;900&display=swap"
+        />
+      </Head>
       <body className="raleway">
         <NavBar />
         {children}
